491b572e9dc0a5256facbd365c2252efbb4cf51e
Rit19081 <your-email@example.com>
2025-09-06 09:17:40 +0530

    cron.js added

--- src/server.js
+++ src/server.js
@@ -3,15 +3,22 @@ import dotenv from "dotenv";
 import { initDB } from "./config/db.js"; 
 import rateLimiter from "./middleware/rateLimiter.js";
 import transactionsRoute from "./routes/transactionsRoute.js";
+import job from "./config/cron.js";
+
+// Start the cron job
+if (process.env.NODE_ENV === "production") job.start();
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+app.get("/api/health", (req, res) => {
+    res.status(200).send({ status: "ok" });
+});
 app.use("/api/transactions", transactionsRoute);
 
-app.use(rateLimiter)
+app.use(rateLimiter);
 app.use(express.json());
 
 
